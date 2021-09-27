7eed88c161c4e5c2685da52506aab83f58435333
金沢　千秋 <kanazawachiaki@kanazawachiakinoMacBook-Pro.local>
2021-09-27 17:11:55 +0900

    web版途中

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future main() async {
+  //Firebaseの初期化 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,89 +19,89 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'NexTalk'),
+      home: MyAuthPage(),
     );
   }
 }
 
 //ステートフルはビルドされた後でもユーザーのインタラクションでデータが変わり際描画される部分がある。
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class MyAuthPage extends StatefulWidget {
+  const MyAuthPage({Key? key,}) : super(key: key);
   //下のMyhomepagestateクラスを継承している。
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyAuthPage> createState() => _MyAuthPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAuthPageState extends State<MyAuthPage> {
 
-  void _incrementCounter() {
-    setState(() {
-      //この中に状態の更新処理を書く
-      _counter++;
-    });
-  }
-
+  // 入力されたメールアドレス
+  String newUserEmail = "";
+  // 入力されたパスワード
+  String newUserPassword = "";
+  // 登録・ログインに関する情報を表示
+  String infoText = "";
+    
   @override
   Widget build(BuildContext context) {
     //ページはScaffoldで囲む
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: const <Widget>[
-            UserAccountsDrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(40),
-                bottomRight: Radius.circular(40),
-              )),
-            currentAccountPicture: Icon(Icons.account_circle, size: 64), //タップして画像設定　写真ライブラリから選択？
-            accountEmail: Text('dev.test@gmail.com'),
-            accountName: Text('Dev test'),
-            ),
-            ListTile(
-              leading: Icon(Icons.phone_in_talk),
-              title: Text('通話'),
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('チャット'),
-            ),
-            ListTile(
-              leading: Icon(Icons.groups),
-              title: Text('フレンド'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('設定'),
-            ),
-          ],
-        ),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'クリックカウント',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                //テキスト入力のラベルを設定
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged : (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード(6文字以上)"),
+                //パスワードを見えなくする処理
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    //メール/パスワードでユーザー登録
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                      await auth.createUserWithEmailAndPassword(
+                        email: newUserEmail,
+                        password: newUserPassword,
+                      );
+
+                    //登録したユーザー情報
+                    final User user = result.user!;
+                    setState(() {
+                      infoText = "登録OK:${user.email}";
+                    });
+                  } catch (e) {
+                    //登録に失敗した場合
+                    setState(() {
+                      infoText = "登録NG:${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("ユーザー登録"),
+              ),
+              const SizedBox(height: 8),
+              Text(infoText)
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
